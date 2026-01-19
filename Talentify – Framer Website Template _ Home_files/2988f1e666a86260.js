25814adc3e67dced64e65f55125a0fc90054c2db
arsbux <franciskatale87@gmail.com>
2026-01-19 18:15:09 +0300

    Initial commit: Framer website for Vercel deployment

---
+++ Talentify – Framer Website Template _ Home_files/2988f1e666a86260.js
@@ -0,0 +1,3 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,227428,e=>{"use strict";e.s([],448507),e.i(448507);var o=e.i(285690);e.s(["TimezoneSelect",()=>o.TimezoneSelect,"TimezoneSelectComponent",()=>o.TimezoneSelectComponent],227428)},124879,e=>{e.n(e.i(227428))}]);
+
+//# sourceMappingURL=4c3049d883dbfe4c.js.map
